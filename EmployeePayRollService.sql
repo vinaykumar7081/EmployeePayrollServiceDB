e463917de05c1bb4147626b9d454191cc6069cd2
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-05-31 12:02:08 +0530

    [VINAY] UC2_CtreateTable: Creating table in data base PayRoll Service

--- EmployeePayRollService.sql
+++ EmployeePayRollService.sql
@@ -1,3 +1,8 @@
 Create Database Payroll_Service;
 
+Use Payroll_Service;
 
+Create table Employee_Payroll(Id int  NOT NULL primary key IDENTITY(101,1),Name varchar(35) NOT NULL,Salary Money NOT NULL, StartDate Date NOT NULL
+);
+
+select *from Employee_Payroll;
