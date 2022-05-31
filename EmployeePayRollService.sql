34fe1df573b93d93c8cce7b9797636634a6f8d61
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-05-31 12:16:02 +0530

    [VINAY] UC4_DisplayTableData: Displaying all data present in the table

--- EmployeePayRollService.sql
+++ EmployeePayRollService.sql
@@ -5,7 +5,7 @@ Use Payroll_Service;
 Create table Employee_Payroll(Id int  NOT NULL primary key IDENTITY(101,1),Name varchar(35) NOT NULL,Salary Money NOT NULL, StartDate Date NOT NULL
 );
 
-select *from Employee_Payroll;
+
 
 insert into Employee_Payroll 
 values
@@ -18,3 +18,5 @@ values
 ('Harshal',30000,'2022.May.20'),
 ('Chetna',30000,'2022.May.21'),
 ('Abhinav',23000,'2022.APR.13');
+
+select *from Employee_Payroll;
