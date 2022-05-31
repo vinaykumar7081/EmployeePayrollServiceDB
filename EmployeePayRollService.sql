0b9ecdea5ca9cbd12ffbb673d2a13a099c52dfad
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-05-31 12:10:49 +0530

    [VINAY] UC3_InsertData: Inserting data in table Employee payroll

--- EmployeePayRollService.sql
+++ EmployeePayRollService.sql
@@ -6,3 +6,15 @@ Create table Employee_Payroll(Id int  NOT NULL primary key IDENTITY(101,1),Name 
 );
 
 select *from Employee_Payroll;
+
+insert into Employee_Payroll 
+values
+('Anish',30000,'2022.APR.15'),
+('Akash',20000,'2022.MAY.15'),
+('Anurag',22000,'2022.APR.12'),
+('Geetanjali',50000,'2022.01.01'),
+('Babu',22000,'2022.03.15'),
+('Shudhakr',22000,'2022.Jan.01'),
+('Harshal',30000,'2022.May.20'),
+('Chetna',30000,'2022.May.21'),
+('Abhinav',23000,'2022.APR.13');
